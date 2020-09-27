f2f25545ea83394b1ce0d29ddb5ed3f166fd5a53
Kurt Snuggly <unsnugHero@gmail.com>
2020-09-27 02:08:38 -0400

    Forgot to go back to root directory when adding files to staging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DecksComponent } from './components/pages/decks/decks.component';
+import { CardsComponent } from './components/pages/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DecksComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
