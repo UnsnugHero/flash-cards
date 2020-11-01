b838995f91bdedc0af616ca20fa719d5f7cb1ccd
Kurt Snuggly <unsnugHero@gmail.com>
2020-11-01 01:13:48 -0400

    flash card component

--- src/app/components/general/flash-card/flash-card.component.ts
+++ src/app/components/general/flash-card/flash-card.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'flash-card',
   templateUrl: './flash-card.component.html',
-  // styleUrls: ['./name.component.scss']
+  styleUrls: ['./flash-card.component.less'],
 })
 export class FlashCardComponent {
+  public isCardFlipped: boolean = false;
+
+  /** String displayed on the back side of the card */
+  @Input() answer: string;
+  /** string that displays underneath the answer (controlled by button under card that says show mnemonics?) */
+  @Input() mnemonic: string;
+  /** String corresponding displayed on the front of the card */
+  @Input() prompt: string;
+  /** boolean to determine wether to show mnemonic */
+  @Input() showMnemonic: boolean = false;
+
   constructor() {}
 
   ngOnInit() {}
