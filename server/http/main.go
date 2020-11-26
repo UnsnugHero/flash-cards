98d67e720fcb354fd9b84d47c992d8ccf791942e
Kurt Snuggly <unsnugHero@gmail.com>
2020-11-26 01:24:03 -0500

    added a test get route, also script command for building server

--- server/http/main.go
+++ server/http/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello Server!")
+
+	// declare our router. Default() returns a new Engine instance. As opposed to using New(),
+	// Default() attaches Logger and Recovery middleware whereas New() has no middlewhere
+	router := gin.Default()
+
+	// GET routes
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello!!",
+		})
+	})
+
+	// listen and serve on port 8080 default
+	router.Run()
 }
