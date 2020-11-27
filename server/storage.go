9d4de000ca1af88a9c8f801d6e4524e8d8529e57
Kurt Snuggly <unsnugHero@gmail.com>
2020-11-27 12:47:18 -0500

    initial storage declaration

---
+++ server/storage.go
@@ -0,0 +1,43 @@
+package main
+
+// StorageType will identify which storage method we want to use
+type StorageType int
+
+const (
+	jsonStorageLocation = "./json-storage"
+
+	// iota will enumerate for our constants, so JSON = 0, Database = 1...
+
+	// JSON StorageType def
+	JSON StorageType = iota
+	// Database StorageType def
+	Database
+)
+
+// Storage declares method signatures to be implemented by storage type
+type Storage interface {
+	// Deck actions
+	AddDeck() error
+	GetDeck() (Deck, error)
+	GetDecks() ([]Deck, error)
+
+	// Card Actions
+
+	// Category Actions
+}
+
+// NewStorage takes in a storage type and returns a storage client instance of StorageType
+func NewStorage(storageType StorageType) (Storage, error) {
+
+	var storage Storage
+	var err error
+
+	switch storageType {
+	case JSON:
+		// initialize json memory storage here
+	case Database:
+		// initialize database storage here
+	}
+
+	return storage, err
+}
